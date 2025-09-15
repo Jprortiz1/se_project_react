03b0d82cb7c9d2ec3a7134d631baf20c931df79e
Jorge Proaño <j.prortiz1@gmail.com>
2025-09-15 17:18:08 -0400

    Sidebar updated

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,23 @@
 import ClothesSection from "../ClothesSection/ClothesSection";
 import "./Profile.css";
+import Sidebar from "../Sidebar/Sidebar";
 
 function Profile({ user, items, weatherData, onSelectCard, onAddClothes }) {
   return (
     <div className="profile">
       <div className="profile__inner">
+        {/* Columna izquierda */}
         <div className="profile__left">
           <div className="profile__header-card">
             <img className="header__avatar" src={user.avatar} alt={user.name} />
             <p className="header__name">{user.name}</p>
           </div>
+
+          {/* 👉 Sidebar añadido */}
+          <Sidebar />
         </div>
+
+        {/* Columna derecha */}
         <div className="profile__right">
           <div className="profile__right-actions">
             <p className="title">Your Items</p>
@@ -18,6 +25,7 @@ function Profile({ user, items, weatherData, onSelectCard, onAddClothes }) {
               + Add new
             </button>
           </div>
+
           <ClothesSection
             items={items}
             weatherType={weatherData?.type}
